017974f824fbb43976db40f8d03034e0a829db7a
Attila Kelemen <attila.kelemen85@gmail.com>
2014-04-13 18:52:02 +0200

    Added EvaluateUtils.asPath.

---
+++ plugin/src/main/groovy/org/gradle/plugins/nbm/EvaluateUtils.java
@@ -0,0 +1,35 @@
+package org.gradle.plugins.nbm;
+
+import groovy.lang.Closure;
+import groovy.lang.GString;
+import java.io.File;
+import java.net.URI;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public final class EvaluateUtils {
+    public static Path asPath(Object obj) {
+        if (obj instanceof Path || obj == null) {
+            return (Path)obj;
+        }
+        if (obj instanceof File) {
+            return ((File)obj).toPath();
+        }
+        if (obj instanceof URI) {
+            return Paths.get((URI)obj);
+        }
+        if (obj instanceof String || obj instanceof GString) {
+            return Paths.get(obj.toString());
+        }
+        if (obj instanceof Closure) {
+            return asPath(((Closure<?>)obj).call());
+        }
+
+        throw new IllegalArgumentException("Unexpected file type: " + obj.getClass().getName());
+    }
+
+    private EvaluateUtils() {
+        throw new AssertionError();
+    }
+
+}
