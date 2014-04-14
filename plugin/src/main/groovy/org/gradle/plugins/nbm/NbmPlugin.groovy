bc7e5fb48b6f06af4f0c5f00f7cb014e6e92ce13
Attila Kelemen <attila.kelemen85@gmail.com>
2014-04-14 23:40:35 +0200

    NbmPlugin defines run and debug (though, currently debug is the same as run).

--- plugin/src/main/groovy/org/gradle/plugins/nbm/NbmPlugin.groovy
+++ plugin/src/main/groovy/org/gradle/plugins/nbm/NbmPlugin.groovy
@@ -12,9 +12,12 @@ import org.gradle.api.plugins.BasePlugin
 import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.Exec
 import org.gradle.api.tasks.SourceSet
 import org.gradle.api.tasks.compile.JavaCompile
 
+import java.nio.file.Files
+import java.nio.file.Path
 import java.util.concurrent.Callable
 
 public class NbmPlugin implements Plugin<Project> {
@@ -78,6 +81,37 @@ public class NbmPlugin implements Plugin<Project> {
         project.tasks.build.dependsOn(nbmTask)
 
         configureConfigurations(project.configurations)
+        addRunAndDebugTasks(project)
+    }
+
+    private void addRunTask(Project project, String taskName, boolean debug) {
+        project.task([type: Exec], taskName, {
+            dependsOn 'netbeans'
+
+            Path buildPath = project.buildDir.toPath()
+            Path testUserDir = buildPath.resolve('testuserdir')
+            if (project.hasProperty('netBeansExecutable')) {
+                doFirst {
+                    def confFile = testUserDir.resolve('etc').resolve('netbeans.conf')
+                    Files.createDirectories(confFile.parent)
+                    confFile.toFile().write "netbeans_extraclusters=\"${buildPath.resolve('module')}\""
+                }
+
+                // TODO: What to do when debug is true?
+                workingDir project.buildDir
+                commandLine project.netBeansExecutable, '--userdir', testUserDir
+            }
+            else {
+                doFirst {
+                    throw new IllegalStateException('The property netBeansExecutable is not specified, you should define it in ~/.gradle/gradle.properties')
+                }
+            }
+        })
+    }
+
+    private void addRunAndDebugTasks(Project project) {
+        addRunTask(project, 'run', false)
+        addRunTask(project, 'debug', true)
     }
 
     public void configureConfigurations(ConfigurationContainer configurationContainer) {
